a3af4a7a023b3aaad29ddd39b4cb05ae6fae4196
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-03 07:28:28 +0100

    minor adjustments

--- ds_lang/app/main.cpp
+++ ds_lang/app/main.cpp
@@ -12,14 +12,11 @@ int main() {
     using namespace ds_lang;
 
     std::string code = load_code("examples/simple.ds");
+
     Lexer lexer{code};
     std::vector<Token> tokens = lexer.tokenize_all();
     Parser parser{tokens};
-    auto scope = parser.parse_scope();
+
     Interpreter interpreter{false};
-    for (const auto &statement : scope) {
-        if (interpreter.process_statement(statement) != Interpreter::ExecResult::Continue) {
-            break;
-        }
-    }
+    interpreter.process_scope(parser.parse_scope());
 }
