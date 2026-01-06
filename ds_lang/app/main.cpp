bb4755393fed9f057321af0d3a3e8423484c4e0b
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-06 11:09:52 +0100

    Cleanup

--- ds_lang/app/main.cpp
+++ ds_lang/app/main.cpp
@@ -13,32 +13,26 @@
 int main() {
     using namespace ds_lang;
 
-    // Load source
-    std::string code = load_code("examples/simple.ds");
+    std::string code = load_code("examples/func.ds");
 
-    // Lex + parse
     std::vector<Token> tokens = Lexer{code}.tokenize_all();
     std::vector<Statement> statements = Parser{tokens}.parse_program();
 
-    // Dump tokens
     std::println("Tokens:");
     for (const Token& token : tokens) {
         std::println("{}", token);
     }
     std::println();
 
-    // Dump parsed functions (AST)
     std::println("Functions:");
     for (usize i = 0; i < statements.size(); ++i) {
         std::println("[{:03}]\n{}", i, statements[i]);
     }
     std::println();
 
-    // Build bytecode
     BytecodeBuilder builder{};
     builder.build(statements);
 
-    // Dump bytecode
     std::println("Bytecode:");
     for (usize i = 0; i < builder.functions().size(); ++i) {
         const FunctionBytecode& fn = builder.functions()[i];
@@ -51,13 +45,17 @@ int main() {
     }
     std::println();
 
-    // Run VM
+    const auto entry = builder.entry_function();
+    if (!entry) {
+        throw std::runtime_error("BytecodeBuilder did not set an entry function");
+    }
+
     VirtualMachine vm{true};
     for (const auto& fn : builder.functions()) {
         (void)vm.add_function(fn);
     }
 
-    vm.set_entry_function(*builder.entry_function());
+    vm.set_entry_function(*entry);
     vm.reset();
     vm.run();
 
