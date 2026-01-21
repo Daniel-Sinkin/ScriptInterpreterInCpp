64fab6b8774cc830e084a52e18dc8741f922626d
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-21 19:42:24 +0100

    Made bytecode running part of the problem

--- ds_lang/app/main.cpp
+++ ds_lang/app/main.cpp
@@ -14,7 +14,7 @@
 void run_struct_example() {
     using namespace ds_lang;
 
-    std::string code = load_code("examples/struct.ds");
+    std::string code = load_code("examples/collatz.ds");
 
     std::vector<Token> tokens = Lexer{code}.tokenize_all();
 
@@ -31,11 +31,43 @@ void run_struct_example() {
     }
     std::println();
     AstDot::write_dot_file("struct.dot", program);
+
+        BytecodeBuilder builder{};
+    builder.build(program);
+
+    std::println("Bytecode:");
+    for (usize i = 0; i < builder.functions().size(); ++i) {
+        const FunctionBytecode& fn = builder.functions()[i];
+        std::println("Function {}:", static_cast<u32>(i));
+        std::println("  num_params = {}", fn.num_params);
+        std::println("  num_locals = {}", fn.num_locals);
+        for (usize ip = 0; ip < fn.code.size(); ++ip) {
+            std::println("  {:>4}: {}", ip, fn.code[ip]);
+        }
+    }
+    std::println();
+
+    const auto entry = builder.entry_function();
+    if (!entry) {
+        throw std::runtime_error("BytecodeBuilder did not set an entry function");
+    }
+
+    VirtualMachine vm{true};
+    for (const auto& fn : builder.functions()) {
+        (void)vm.add_function(fn);
+    }
+
+    vm.set_entry_function(*entry);
+    vm.reset();
+    vm.run();
+
+    std::println("Return Value = {}", vm.get_return_value());
 }
 
 int main() {
     using namespace ds_lang;
-    // run_struct_example();
+    run_struct_example();
+    return 0;
 
     std::string code = load_code("examples/struct.ds");
 
